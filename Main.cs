11bd2f2eb440cb26ebd5e828cceb8697dc6badc3
rusgord <149193817+rusgord@users.noreply.github.com>
2024-11-22 14:55:27 +0200

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,79 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	[Export]
+	public PackedScene MobScene { get; set; }
+
+	private int _score;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		NewGame();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public void GameOver()
+	{
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Timer>("ScoreTimer").Stop();
+	}
+
+	public void NewGame()
+	{
+		_score = 0;
+
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartedPosition");
+		player.Start(startPosition.Position);
+
+		GetNode<Timer>("StartTimer").Start();
+		}
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnScoreTimerTimeout()
+	{
+		_score++;
+	}
+
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnMobTimerTimeout()
+	{
+		// Note: Normally it is best to use explicit types rather than the `var`
+		// keyword. However, var is acceptable to use here because the types are
+		// obviously Mob and PathFollow2D, since they appear later on the line.
+
+		// Create a new instance of the Mob scene.
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		// Choose a random location on Path2D.
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		// Set the mob's direction perpendicular to the path direction.
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+		// Set the mob's position to a random location.
+		mob.Position = mobSpawnLocation.Position;
+
+		// Add some randomness to the direction.
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		mob.Rotation = direction;
+
+		// Choose the velocity.
+		var velocity = new Vector2((float)150.0, 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
+	}
+}
